3d5835d96f13eaf789d7ecb0c1c5dedfac7326cb
Kunal <nagwanshikunal23@gmail.com>
2023-07-13 12:59:07 +0530

    daemon thread 2

--- runnable/DaemonThread.java
+++ runnable/DaemonThread.java
@@ -1,2 +1,36 @@
-package com.runnable;public class DaemonThread {
+package com.runnable;
+
+
+
+
+class Example1 implements Runnable{
+    public void run(){
+
+        String name = Thread.currentThread().getName();
+        System.out.println("just i want to check what daemon thread will do "+name);
+
+    }
+}
+
+public class DaemonThread {
+    public static void main(String [] args){
+        boolean on = true;
+        Example1 ex = new Example1();
+        Thread t1 = new Thread(ex);
+        Thread t2 = new Thread(ex);
+        Thread t3 = new Thread(ex);
+
+        t1.setDaemon(on);
+        t2.setDaemon(on);
+        t3.setDaemon(on);
+
+        t1.setName("thread1");
+        t2.setName("thread2");
+        t3.setName("thread3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+    }
 }
